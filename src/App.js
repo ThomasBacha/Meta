64732fa2922f877c64f62b1c1eeee4c5e785b78c
Bacha <thomas.bacha@capgemini.com>
2023-06-02 10:16:05 -0500

    Adding everything for day1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import Main from './components/Main.js';
+import Nav from './components/Nav.js';
 
 function App() {
   return (
-    <div className="App">
-      HomePage <br />
-      {/* Adding some text to fill in the page */}
-      This is the home page of the website.
-      <br />
-      <br />
-      <br />
-      This is a default text.
-    </div>
+ <>
+      <Header />
+      <Nav />
+      <Main>
+        <h1>HomePage</h1>
+        {/* Adding some text to fill in the page */}
+        <p>This is the home page of the website.</p>
+        <p>This is a default text.</p>
+      </Main>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+      {/* { Test one to test the nav bar} */}
+    </nav>
+  );
+}
+
+export default Nav;
