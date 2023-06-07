dba3a0c6581fa455c4f13a66e6ddeb1989017eb4
Bacha <thomas.bacha@capgemini.com>
2023-06-07 12:10:25 -0500

    adding all to push

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,26 @@
 import CommonHeader from "./CommonHeader";
+import CenteredImage from "./Construction";
 
 function Login(){
     return (
         <>
             <CommonHeader
                 title={"Login"}/>
-            <p>Login</p>
+            {/* Style the login to be in the center of page */}
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <form>
+                    <label>
+                        Username:
+                        <input type="text" name="name" />
+                    </label>
+                    <label>
+                        Password:
+                        <input type="text" name="name" />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+            <CenteredImage altText={"Under Construction"}/>
         </>
     );
 }
